af4ec0bccc53ad9181c652b8a17d4a24f39d3e29
Filipe Silva <sousadasilvafilipe@gmail.com>
2021-11-18 17:23:45 -0300

    fazendo iteração com teclado fisico

--- script.js
+++ script.js
@@ -97,3 +97,33 @@ const inserirDecimal = () => {
 }
 document.getElementById('decimal').addEventListener('click', inserirDecimal);
 
+const mapaTeclado = {
+  '0'        : 'tecla0',
+  '1'        : 'tecla1',
+  '2'        : 'tecla2',
+  '3'        : 'tecla3',
+  '4'        : 'tecla4',
+  '5'        : 'tecla5',
+  '6'        : 'tecla6',
+  '7'        : 'tecla7',
+  '8'        : 'tecla8',
+  '9'        : 'tecla9',
+  '/'        :'operadorDividir',    
+  '*'        :'operadorMultiplicar',
+  '-'        :'operadorSubtrair',
+  '+'        :'operadorAdicionar',
+  '='        :'igual',
+  'Enter'    :'igual',
+  'BacKspace':'backspace',
+  'c'        :'limparDisplay',
+  'Escape'   :'limparCalculo',
+  ','        :'decimal'
+}
+
+const mapearTeclado = (evento) => {
+  const tecla = evento.key;
+  console.log(evento.key);
+const  teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) != -1;
+ if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+}
+document.addEventListener('keydown', mapearTeclado)
